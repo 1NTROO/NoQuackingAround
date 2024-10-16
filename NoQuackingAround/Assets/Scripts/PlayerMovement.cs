b8b86cc8b33cb16b55306d8c152fd837563ac838
1NTROO <95148848+1NTROO@users.noreply.github.com>
2024-10-16 11:38:03 +0200

    player movement + some folders
    basic player movement (not perfect)
    added some folders that will be useful later

---
+++ NoQuackingAround/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] int speed;
+    [SerializeField] int speedLimit;
+    [SerializeField] Rigidbody2D body;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Vector3 moveVector = new Vector3(0, 0, 0);
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveVector.x += -speed;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveVector.x += speed;
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveVector.y += speed;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveVector.y += -speed;
+        }
+
+        body.AddRelativeForce(moveVector);
+        
+    }
+}
